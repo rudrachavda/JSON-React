986d0f62e4e838df7746fc162a00cf8d722cda2e
Rudra Chavda <rudrachavda@Rudras-Air.lan>
2023-06-27 17:05:31 -0700

    square root website added plus html plus css abd js

---
+++ basic-test/test.js
@@ -0,0 +1,8 @@
+function calculateSquare() {
+    var n = parseInt(document.getElementById("input-n").value);
+    console.log("clicked on calculate ",n)
+    var squareResult = n * n;
+    
+    document.getElementById("n-value").textContent = n;
+    document.getElementById("square-value").textContent = squareResult;
+  }
--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editing this app can be kind of cool
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Learn React
         </a>
       </header>
     </div>
